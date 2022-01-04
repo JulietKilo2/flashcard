0a5d51f696af25c1707a841f3be0b6ac85b51ef8
Jisub <jisubkim02@gmail.com>
2022-01-04 16:59:46 +0900

    프로젝트 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>플래시카드</h1>
       </header>
+      <nav>
+        <ul>
+          <li>대시보드</li>
+          <li>라이브러리</li>
+          <li>만들기</li>
+        </ul>
+      </nav>
+      <div>단어장 목록</div>
     </div>
   );
 }
