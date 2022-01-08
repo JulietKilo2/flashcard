8065a9097b66531464b8a773663bcac7b53b9307
jisub <jisubkim02@gmail.com>
2022-01-08 18:36:06 +0900

    라이브러리 UI 추가

--- src/components/library/Library.jsx
+++ src/components/library/Library.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import "./Library.scss"
 
 export default function Library({ data }) {
   return (
     <div>
-      <h1>라이브러리에요.</h1>
+      <h1>라이브러리</h1>
+      <div className="library-container">
       {data &&
         data.map((item) => {
           return (
-            <div key={item.id}>
+            <div key={item.id} className="library-card">
               <h2>{item.name}</h2>
-              <p>{item.items.length}</p>
+              <p>단어: {item.items.length}개</p>
+              <div className="library-btn-container">
+                <button className="library-btn">테스트</button>
+                <button className="library-btn">수정</button>
+                <button className="library-btn">삭제</button>
+              </div>
             </div>
           );
         })}
+      </div>
     </div>
   );
 }
